eec58bfe70afe9f007f9f4d4c16d9e0ba986f1a8
Kelley Jenkins <ckjenkins3@gmail.com>
2018-01-22 22:26:12 -0700

    created transaction show and index routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,10 @@ Rails.application.routes.draw do
         get 'find', to: 'search#show'
         get 'find_all', to: 'search#index'
         get 'random', to: 'random#show'
-
       end
+
       resources :merchants, only: [:index, :show]
+      resources :transactions, only: [:index, :show]
     end
   end
 end
