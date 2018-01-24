ca9ee1544289bec0cb0e0d75d7d46541fae2701d
Kelley Jenkins <ckjenkins3@gmail.com>
2018-01-24 12:00:30 -0700

    defined scope

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,8 @@
 class Transaction < ApplicationRecord
   belongs_to :invoice
 
+  scope :successful, -> {where(result: '0')}
+  scope :not_successful, -> {where(result: '1')}
+
+  default_scope {order(:id)}
 end
