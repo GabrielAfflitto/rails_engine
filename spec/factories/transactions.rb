435c8609dcff02201467e2e384672dce91a98f72
Kelley Jenkins <ckjenkins3@gmail.com>
2018-01-24 14:18:47 -0700

    updated transactions factory to reflect updated schema

--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :transaction do
     credit_card_number "4563849302031045"
     credit_card_expiration_date "3/15/18"
-    result 1
+    result "success"
     invoice
   end
 end
