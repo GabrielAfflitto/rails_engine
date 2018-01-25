1dafa38d05e4f5f979fe2dbe1fb85e14b1b4689a
Kelley Jenkins <ckjenkins3@gmail.com>
2018-01-25 09:14:06 -0700

    added route for invoices/transactions:

--- config/routes.rb
+++ config/routes.rb
@@ -31,6 +31,7 @@ Rails.application.routes.draw do
         get 'find', to: 'search#show'
         get 'find_all', to: 'search#index'
         get 'random', to: 'random#show'
+        get ':id/transactions', to: 'transactions#show'
       end
 
       namespace :items do
