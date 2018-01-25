516f032368e2dd37ecdf73d8da44312860f7a5b1
Gabriel Afflitto <gafflitto92@gmail.com>
2018-01-25 08:50:52 -0700

    Transactions enpoint, returns the asscociated invoice for transaction

--- spec/requests/api/v1/transaction_records_spec.rb
+++ spec/requests/api/v1/transaction_records_spec.rb
@@ -1,64 +1,82 @@
 require 'rails_helper'
 
 describe "Transactions API" do
-  it "returns a list of transactions" do
-    invoice = create(:invoice)
-    transaction1 = create(:transaction, invoice:invoice)
-    transaction2 = create(:transaction, invoice:invoice)
-    transaction3 = create(:transaction, invoice:invoice)
+  context "Record endpoints" do
 
-    get '/api/v1/transactions'
+    it "returns a list of transactions" do
+      invoice = create(:invoice)
+      transaction1 = create(:transaction, invoice:invoice)
+      transaction2 = create(:transaction, invoice:invoice)
+      transaction3 = create(:transaction, invoice:invoice)
 
-    expect(response).to be_success
+      get '/api/v1/transactions'
 
-    transactions = JSON.parse(response.body)
+      expect(response).to be_success
 
-    expect(transactions.count).to eq(3)
+      transactions = JSON.parse(response.body)
+
+      expect(transactions.count).to eq(3)
+    end
+
+    it "can return one transaction by it's id" do
+
+      invoice = create(:invoice)
+      id = create(:transaction, invoice: invoice).id
+
+      get "/api/v1/transactions/#{id}"
+
+      transaction = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(transaction["id"]).to eq(id)
+    end
+
+    it "can find one transaction by it's credit_card_number" do
+      card = create(:transaction ).credit_card_number
+
+      get "/api/v1/transactions/find?credit_card_number=#{card}"
+
+      transaction = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(transaction["credit_card_number"]).to eq(card)
+    end
+
+    it "can find all transactions by parameters" do
+      transaction1 , transaction2, transaction3 = create_list(:transaction, 3)
+
+      get "/api/v1/transactions/find_all?credit_card_number=#{transaction1.credit_card_number}"
+
+      transaction = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(transaction.first["credit_card_number"]).to eq(transaction1.credit_card_number)
+      expect(transaction.count).to eq(3)
+    end
+
+    it "can find a random transaction" do
+      create_list(:transaction, 3)
+
+      get "/api/v1/transactions/random"
+
+      transaction = JSON.parse(response.body)
+
+      expect(response).to be_success
+    end
   end
 
-  it "can return one transaction by it's id" do
+  context "Relationship endpoints" do
+    it "loads the associated invoice for the transaction" do
+      invoice = create(:invoice)
+      invoice2 = create(:invoice)
+      transaction = create(:transaction, invoice: invoice)
 
-    invoice = create(:invoice)
-    id = create(:transaction, invoice: invoice).id
+      get "/api/v1/transactions/#{transaction.id}/invoice"
 
-    get "/api/v1/transactions/#{id}"
-
-    transaction = JSON.parse(response.body)
-
-    expect(response).to be_success
-    expect(transaction["id"]).to eq(id)
-  end
-
-  it "can find one transaction by it's credit_card_number" do
-    card = create(:transaction ).credit_card_number
-
-    get "/api/v1/transactions/find?credit_card_number=#{card}"
-
-    transaction = JSON.parse(response.body)
-
-    expect(response).to be_success
-    expect(transaction["credit_card_number"]).to eq(card)
-  end
-
-  it "can find all transactions by parameters" do
-    transaction1 , transaction2, transaction3 = create_list(:transaction, 3)
-
-    get "/api/v1/transactions/find_all?credit_card_number=#{transaction1.credit_card_number}"
-
-    transaction = JSON.parse(response.body)
-
-    expect(response).to be_success
-    expect(transaction.first["credit_card_number"]).to eq(transaction1.credit_card_number)
-    expect(transaction.count).to eq(3)
-  end
-
-  it "can find a random transaction" do
-    create_list(:transaction, 3)
-
-    get "/api/v1/transactions/random"
-
-    transaction = JSON.parse(response.body)
-
-    expect(response).to be_success
+      inv = JSON.parse(response.body)
+      expect(response).to be_successful
+      expect(transaction.invoice.id).to eq(invoice.id)
+      expect(transaction.invoice.id).to_not eq(invoice2.id)
+    end
   end
 end
