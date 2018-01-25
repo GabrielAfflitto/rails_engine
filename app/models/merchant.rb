34182aefb2d5b14bd940e347fa3f6b7d1ef794c6
Gabriel Afflitto <gafflitto92@gmail.com>
2018-01-24 22:47:54 -0700

    merchants endpoint, returns all associated invoices

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,6 +2,7 @@ class Merchant < ApplicationRecord
   has_many :invoices
   has_many :items
   has_many :customers, through: :invoices
+  has_many :transactions, through: :invoices
 
   def total_revenue
     {revenue: (invoices.joins(:invoice_items, :transactions).where("transactions.result = 'success'").sum("invoice_items.quantity*invoice_items.unit_price")/ 100.0).to_s}
