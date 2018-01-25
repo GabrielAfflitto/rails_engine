7bc2b29b54016302dfe92925ac284b2a87eb891c
Gabriel Afflitto <gafflitto92@gmail.com>
2018-01-24 17:23:15 -0700

    top merchants ranked by total revenue created, tested and passing

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,9 +1,22 @@
 class Merchant < ApplicationRecord
   has_many :invoices
   has_many :items
+  has_many :customers, through: :invoices
 
   def total_revenue
     {revenue: (invoices.joins(:invoice_items, :transactions).where("transactions.result = 'success'").sum("invoice_items.quantity*invoice_items.unit_price")/ 100.0).to_s}
+  end
+
+  def self.top_merchants_by_revenue(limit = 4)
+    select("merchants.*, sum(invoice_items.quantity*invoice_items.unit_price) AS revenue")
+    .joins(invoices: [:invoice_items, :transactions])
+    .where("transactions.result = 'success'")
+    .group(:id)
+    .order("revenue DESC").limit(limit)
+  end
+
+  def self.top_merchants_by_items_sold
+    select
   end
 
 end
