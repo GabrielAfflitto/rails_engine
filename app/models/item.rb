19bacedc2846810e4979ad7f00db9485f660247a
Gabriel Afflitto <gafflitto92@gmail.com>
2018-01-26 07:39:47 -0700

    replaced .where with .merge for queries in item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,15 +6,25 @@ class Item < ApplicationRecord
   default_scope {order(:id)}
 
   def self.top_items_by_revenue(limit =4)
-    unscoped.select("items.*, sum(invoice_items.quantity*invoice_items.unit_price) AS revenue").joins(invoices: [:invoice_items, :transactions]).where("transactions.result = 'success'").group(:id).order("revenue DESC").limit(limit)
+    unscoped.select("items.*, sum(invoice_items.quantity*invoice_items.unit_price) AS revenue")
+    .joins(invoices: [:invoice_items, :transactions])
+    .merge(Transaction.unscoped.successful)
+    .group(:id).order("revenue DESC").limit(limit)
   end
 
   def self.top_item_by_number_sold(limit = 4)
-    unscoped.select("items.*, sum(invoice_items.quantity) AS total").joins(invoices:[ :transactions]).where("transactions.result = 'success'").group(:id).order("total DESC").limit(limit)
+    unscoped.select("items.*, sum(invoice_items.quantity) AS total")
+    .joins(invoices:[ :transactions])
+    .merge(Transaction.unscoped.successful)
+    .group(:id)
+    .order("total DESC").limit(limit)
   end
 
   def best_day
-    invoices.joins(:invoice_items, :transactions).where("transactions.result = 'success'").group(:id).order("sum(invoice_items.quantity*invoice_items.unit_price) DESC, invoices.created_at DESC").first.created_at
+    invoices.joins(:invoice_items, :transactions)
+    .merge(Transaction.unscoped.successful)
+    .group(:id)
+    .order("sum(invoice_items.quantity*invoice_items.unit_price) DESC, invoices.created_at DESC").first.created_at
   end
 
 end
