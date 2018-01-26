f68eeee57de555d8d47d275c43fe3c2256fbf038
Gabriel Afflitto <gafflitto92@gmail.com>
2018-01-26 07:45:09 -0700

    replaced .where with .merge for queries in merchant.rb

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -5,20 +5,24 @@ class Merchant < ApplicationRecord
   has_many :customers, through: :invoices
 
   def total_revenue
-    {revenue: (invoices.joins(:invoice_items, :transactions).where("transactions.result = 'success'").sum("invoice_items.quantity*invoice_items.unit_price")/ 100.0).to_s}
+    {revenue: (invoices.joins(:invoice_items, :transactions)
+      .merge(Transaction.unscoped.successful)
+      .sum("invoice_items.quantity*invoice_items.unit_price")/ 100.0).to_s}
   end
 
   def total_revenue_by_date(date)
     selected_date = DateTime.parse(date)
+
     {revenue: (invoices.joins(:invoice_items, :transactions)
-      .where("transactions.result = 'success'")
+      .merge(Transaction.unscoped.successful)
       .where("invoices.created_at" => selected_date.beginning_of_day..selected_date.end_of_day)
       .sum("invoice_items.quantity*invoice_items.unit_price")/ 100.0).to_s}
   end
 
   def favorite_customer
+
     customers.joins(invoices: [:transactions])
-    .where("transactions.result = 'success'")
+    .merge(Transaction.unscoped.successful)
     .group("customers.id")
     .order("count(customers.id) DESC").first
   end
@@ -26,23 +30,26 @@ class Merchant < ApplicationRecord
   def self.top_merchants_by_revenue(limit = 4)
     select("merchants.*, sum(invoice_items.quantity*invoice_items.unit_price) AS revenue")
     .joins(invoices: [:invoice_items, :transactions])
-    .where("transactions.result = 'success'")
+    .merge(Transaction.unscoped.successful)
     .group(:id)
     .order("revenue DESC").limit(limit)
   end
 
   def self.top_merchants_by_items_sold(limit = 4)
-    select("merchants.*, sum(invoice_items.quantity) AS total").joins(invoices:[:invoice_items, :transactions]).where("transactions.result = 'success'").group(:id).order("total DESC").limit(limit)
+    select("merchants.*, sum(invoice_items.quantity) AS total")
+    .joins(invoices:[:invoice_items, :transactions])
+    .merge(Transaction.unscoped.successful)
+    .group(:id)
+    .order("total DESC").limit(limit)
   end
 
   def self.all_revenue_for_date(date)
     selected_date = DateTime.parse(date)
 
     (Invoice.joins(:invoice_items, :transactions)
-      .where("transactions.result = 'success'")
+      .merge(Transaction.unscoped.successful)
       .where("invoices.created_at" => selected_date.beginning_of_day..selected_date.end_of_day)
       .sum("invoice_items.quantity*invoice_items.unit_price")/ 100.0).to_s
-      #refactored method
   end
 
 end
