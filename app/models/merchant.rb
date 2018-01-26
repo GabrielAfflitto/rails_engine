b4c74c6d81281fb5e2ac2069b73463d939493dc1
Gabriel Afflitto <gafflitto92@gmail.com>
2018-01-26 07:58:48 -0700

    moved hash formatting from controller to merchant.rb

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -46,10 +46,10 @@ class Merchant < ApplicationRecord
   def self.all_revenue_for_date(date)
     selected_date = DateTime.parse(date)
 
-    (Invoice.joins(:invoice_items, :transactions)
+    {total_revenue: (Invoice.joins(:invoice_items, :transactions)
       .merge(Transaction.unscoped.successful)
       .where("invoices.created_at" => selected_date.beginning_of_day..selected_date.end_of_day)
-      .sum("invoice_items.quantity*invoice_items.unit_price")/ 100.0).to_s
+      .sum("invoice_items.quantity*invoice_items.unit_price")/ 100.0).to_s}
   end
 
 end
